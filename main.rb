58c81827e9dd30365f6faf5f84b4d17cfaec4b0d
Medafu <justrayzzin@gmail.com>
2022-07-27 19:03:49 +0300

    Works with full sentences now

--- main.rb
+++ main.rb
@@ -2,28 +2,28 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 
 def substrings(string, dict)
 
-  # word_arr = string.downcase.split(" ")
-  # word_arr.map! do |word|
-  #   word = word.split("").map! do |char|
-  #     unless char >= 'a' && char <= 'z'
-  #       char = ""
-  #       char
-  #     end
-  #     char
-  #   end
-  #   word.join("")
-  # end
+  word_arr = string.downcase.split(" ")
+  word_arr.map! do |word|
+    word = word.split("").map! do |char|
+      unless char >= 'a' && char <= 'z'
+        char = ""
+        char
+      end
+      char
+    end
+    word.join("")
+  end
 
   dict.reduce(Hash.new(0)) do |result, elem|
-    if elem.length > string.length
-      findResult = Regexp.new(string) =~ elem
-    else
-      findResult = string =~ Regexp.new(elem)
-    end
+    for word in word_arr do
+      findResult = word =~ Regexp.new(elem)
 
-    unless findResult == nil
-      result[elem] += 1
+      unless findResult == nil
+        result[elem] += 1
+      end
     end
     result
   end
 end
+
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
